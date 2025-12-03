0b9883b625bf966d9969fd0ddf00c8935e6661f7
Júlia <juliammarques05@gmail.com>
2025-12-02 21:02:54 -0300

    ligações de urls, views e post_list.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.contrib.auth.models import User
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    users = User.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
