71270476b6efdebb393336d8edc32f2486ac2bf4
Júlia <juliammarques05@gmail.com>
2025-12-09 20:57:51 -0300

    envia arquivos.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
-<<<<<<< HEAD
 from . import views
-=======
-from . import views 
->>>>>>> a405d1494648b5ad3a19afba839c923370898106
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import  Post
 from .forms import PostForm
-=======
-from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
-from django.contrib.auth.models import User
->>>>>>> a405d1494648b5ad3a19afba839c923370898106
 
 # Create your views here.
 def post_list(request):
-<<<<<<< HEAD
     post = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': post})
     
@@ -27,8 +19,3 @@ def post_new(request):
       form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-=======
-    posts = Post.objects.all()
-    users = User.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
->>>>>>> a405d1494648b5ad3a19afba839c923370898106
