488aa233ca6ad30d0658e0e385f0544b0ebb1db2
Júlia <juliammarques05@gmail.com>
2025-12-09 20:32:27 -0300

    Atualizações recentes dos tempates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import  Post
+from .forms import PostForm
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    post = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': post})
+    
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    if request.method == "POST":
+        [form] = PostForm(request.POST)
+    else:
+      form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
